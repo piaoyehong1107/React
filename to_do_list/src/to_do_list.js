b498b8300c8f17f9fba58a738adf3f4ead552ddc
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2021-05-24 23:36:21 -0500

    1st

---
+++ to_do_list/src/to_do_list.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class to_do_list extends Component {
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form>
+            <input placeholder="enter task">
+            </input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+ 
+export default to_do_list;
