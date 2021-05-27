7f8e0ad75a2b6e4cd36d1aa83bd2281093687e90
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2021-05-26 23:43:43 -0500

    3rd

--- to_do_list/src/TodoList.js
+++ to_do_list/src/TodoList.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
+
  
 class TodoList extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      typeValue: "",
+      items: []
+    };
+  }
+  
+  handleInput = (typedValue) =>{
+    this.setState({
+      typeValue: typedValue
+    })
+  }
+  addItem = () =>{
+    var typevalue2= this.state.typeValue
+    console.log(typevalue2)
+    this.setState({
+      items: [...this.state.items, typevalue2]
+    })
+  }
+
   render() {
+    console.log(this.state.typeValue)
+    console.log(this.state.items)
+    
     return (
-      <div className="TodoListMain">
-        <form>
-            <input placeholder="enter task">
-            </input>
-            <button type="submit">add</button>
+      <div className="todoListMain">
+        <div className="header">
+          <form>
+            <input placeholder="enter task" 
+              onChange={(e) => this.handleInput(e.target.value)}
+            ></input>
+            <button onClick={this.addItem}>ADD</button>
           </form>
+        </div>
       </div>
     );
   }
